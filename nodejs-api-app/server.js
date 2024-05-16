0e2cd831a0f4b63c4a95f8c69c9bbf2d004e4d2c
TXG Ryan <ryan.brown@txg.ie>
2024-05-16 19:55:36 +0100

    Update server.js

--- nodejs-api-app/server.js
+++ nodejs-api-app/server.js
@@ -34,5 +34,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
